a4a4fa7a358315fffb99c661725e8f8c034a42fe
SoraKy0 <KshananthJ@outlook.com>
2024-02-21 08:53:53 +0000

    Triangles Added for Mesh

--- Universe Simulator/Assets/Scripts/Terrain Generator.cs
+++ Universe Simulator/Assets/Scripts/Terrain Generator.cs
@@ -26,7 +26,7 @@ public class TerrainGenerator : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
     }
 
-    private void GenerateTerrain() //Creates Vectors
+    private void GenerateTerrain() //Creates vertices for the mesh
     {
         vertices = new Vector3[(xSize + 1) * (zSize + 1)]; 
 
@@ -40,10 +40,27 @@ public class TerrainGenerator : MonoBehaviour
             }
         }
 
+        int[] triangles = new int[xSize * zSize * 6];
+
+        for(int z = 0; z < zSize: z++)
+        {
+            for(int x = 0: x < xSize; xSize++)     
+            {
+                triangles[0] = 0;
+                triangles[0] = 0;
+                triangles[0] = 0;
+
+                triangles[0] = 0;
+                triangles[0] = 0;
+                triangles[0] = 0;
+            }       
+        }
+
+
         mesh.vertices = vertices;
     }
 
-    private void OnDrawGizmos() //Draws Spheres at each Vectors to show 
+    private void OnDrawGizmos() //Draws Spheres at each vertices to show 
     {
         foreach (Vector3 pos in vertices)
         {
